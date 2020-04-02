0aecac8a1adfe853d9060defbdff1c412687f20f
Stacey Teltser <stacey.teltser@gmail.com>
2020-04-01 22:39:50 -0500

    refactored to include template

--- js/app.js
+++ js/app.js
@@ -1,76 +1,71 @@
 'use strict';
 
 const pics = [];
-let keywords = [];
-let horns = [];
+const keywords = [];
+// let horns = [];
 
-function Pic(url, title, description, keyword, horns) {
-  this.url = url;
-  this.title = title;
-  this.description = description;
-  this.keyword = keyword;
-  this.horns = horns;
+function Pic(pic) {
+  this.image_url = pic.image_url;
+  this.title = pic.title;
+  this.description = pic.description;
+  this.keyword = pic.keyword;
+  this.horns = pic.horns;
+  pics.push(this);
 }
 
+Pic.prototype.render = function (container) {
+  let $container = $(container);
+  let $template = $('#photo-template');
+  let $pic = $template.clone();
+  $pic.removeAttr('id');
+  $pic.find('h2.pic-name').text(this.title);
+  $pic.find('img.pic-display').attr('src', this.image_url);
+  $pic.find('p').text(this.description);
+  $container.append($pic);
+};
 
+function filterPics(pic){
+  let $filter = $('.filter');
+  let $makeFilter = $('<option>');
+  $makeFilter.text(pic.keyword);
+  $makeFilter.val(pic.keyword);
+
+  if (!keywords.includes(pic.keyword)) {
+    keywords.push(pic.keyword);
+    $filter.append($makeFilter);
+  }
+}
 const ajaxSettings = {
   method: 'get',
   dataType: 'json'
 };
 
-$.ajax('data/page-1.json', ajaxSettings)
-  .then(function(data) {
-    let $data = data;
-    $data.forEach(function(object){
-      pics.push(new Pic(object.image_url, object.title, object.description, object.keyword, object.horns));
-      keywords.push(object.keyword);
-      horns.push(object.horns);
-    });
-    pics.forEach(function(object){
-      renderimage(object.url, object.title, object.description, object.horns, object.keyword);
-    });
-    keywords = new Set(keywords);
-    keywords.forEach(function(object){
-      filterByKeyword(object);
-    });
-    horns = new Set(horns);
-    horns.forEach(function(object){
-      filterByHorns(object);
-    });
-    $('select').change(hideElement);
-    // $('select').change(hideHorns);
+let imgs = null;
+$.ajax('data/page-1.json', ajaxSettings).then(function(data) {
+  imgs = data;
+  renderElement('default');
+  imgs.forEach(pic => filterPics(pic));
+
+});
+
+function renderElement(filter) {
+  $('main').empty();
+  imgs.forEach((pic) => {
+    let displayPic = new Pic(pic);
+    if (displayPic.keyword === filter) {
+      displayPic.render('main');
+    } else if (filter === 'default') {
+      displayPic.render('main');
+    }
   });
-
-function renderimage(url, title, description, horns, keyword) {
-  let $section = $('<section>').attr('data-keyword', keyword);
-  let $title = $('<h2>').text(title);
-  let $img = $('<img>').attr('src', url).attr('alt', description);
-  let $text = $('<p>').text(`Image description: ${description}`);
-  let $horns = $('<p>').text(`# of Horns: ${horns}`).attr('data-keyword', horns);
-  $section.append($title, $img, $text, $horns);
-  $('main').append($section);
 }
 
-function filterByKeyword(keyword) {
-  let $option = $('<option>').text(keyword).attr('value', keyword);
-  $('.keyword-filter').append($option);
-}
+$('.filter').on('change', function() {
+  let $this = $(this),
+    filterValue = $this.val();
 
-function filterByHorns(horns) {
-  let $option = $('<option>').text(horns).attr('value', horns);
-  $('.horns-filter').append($option);
-
-}
-
-function hideElement() {
-  let value = $(this).val();
-  if(value !== 'default'){
-    $('section').hide();
-    $(`section[data-keyword=${value}]`).slideDown(900);
-  } else {
-    $('section').fadeIn(800);
-  }
-}
+  renderElement(filterValue);
+});
 
 function reloadPage(){
   location.reload(true);
@@ -87,6 +82,55 @@ function sortAlphabetical(a, b) {
   }
   return comparison;
 }
+
+
+//   $data.forEach(function(pic){
+//     pics.push(new Pic(pic));
+//     keywords.push(pic.keyword);
+//     horns.push(pic.horns);
+//   });
+//   pics.forEach(function(object){
+//     renderimage(object.url, object.title, object.description, object.horns, object.keyword);
+//   });
+//   keywords = new Set(keywords);
+//   keywords.forEach(function(object){
+//     filterByKeyword(object);
+//   });
+//   horns = new Set(horns);
+//   horns.forEach(function(object){
+//     filterByHorns(object);
+//   });
+//   $('select').change(hideElement);
+// $('select').change(hideHorns);
+
+
+
+
+// function filterByKeyword(keyword) {
+//   let $option = $('<option>').text(keyword).attr('value', keyword);
+//   $('.keyword-filter').append($option);
+// }
+
+// function filterByHorns(horns) {
+//   let $option = $('<option>').text(horns).attr('value', horns);
+//   $('.horns-filter').append($option);
+
+// }
+// $('.horns-filter').on('change,', function() {
+//     let $this = $(this),
+//     filterValue = $this.val();
+//     console.log(filterValue);
+// })
+
+// function hideElement() {
+//   let value = $(this).val();
+//   if(value !== 'default'){
+//     $('section').hide();
+//     $(`section[data-keyword=${value}]`).slideDown(900);
+//   } else {
+//     $('section').fadeIn(800);
+//   }
+// }
 
 // pics.sort(sortAlphabetical);
 // $('.sort').on('click',(function(){
